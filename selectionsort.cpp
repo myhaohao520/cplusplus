6390b88ac91e6ed57869d4c0dbcacb937e4a858e
LazyTang <50754881+myhaohao520@users.noreply.github.com>
2023-05-01 23:09:17 -0700

    Update selectionsort.cpp
    Remove useless parts.

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,74 +1,20 @@
-#include <iostream>
-using namespace std;
-
-// function prototypes
-void selectionSortArray(int [], int);
-void displayArray(int[], int);
-
-const int SIZE = 5;
-
-int main()
+void SortArray(int array[], int size)
 {
-    int values[SIZE] = {9,2,0,11,5};
+    int min_index, min_value;
 
-    cout << "The values before the selection sort is performed are:" << endl;
-    displayArray(values,SIZE);
-
-    selectionSortArray(values,SIZE);
-
-    cout << "The values after the selection sort is performed are:" << endl;
-    displayArray(values,SIZE);
-
-    return 0;
-    }
-
-
-// displayArray
-// task: to print the array
-// data in: the array to be printed, the array size
-
-void displayArray(int array[], int elems)
-{
-    for (int count = 0; count < elems; count++)
-        cout << array[count] << " ";
-    cout << endl;
-}
-
-// selectionSortArray
-// task: to sort values of an array in ascending order
-// data in: the array, the array size
-// data out: the sorted array
-
-void selectionSortArray(int array[], int elems)
-{
-    int seek;               // array position currently being put in order
-    int minCount;           // location of smallest value found
-    int minValue;           // holds the smallest value found
-
-    for (seek = 0; seek < (elems-1); seek++)     // outer loop performs the swap and then increments seek
+    for (int start = 0; start < size - 1; start++)
     {
-        minCount = seek;
-        minValue = array[seek];
-        for(int index = seek + 1; index < elems; index++)
+        min_index = start;
+        min_value = array[start];
+        for (int index = start + 1; index < size; index++)
         {
-            // inner loop searches through array starting at array[seek] searching
-            // for the smallest value. When the value is found, the subscript is
-            // stored in minCount. The value is stored in minValue.
-
-            if(array[index] < minValue)
+            if (array[index] < min_value) //for ascending order, replace < with > for descending oder
             {
-                minValue = array[index];
-                minCount = index;
+                min_value = array[index];
+                min_index = index;
             }
         }
-
-        // the following two statements exchange the value of the element currently
-        // the smallest value found in the pass(indicated by seek) with the smallest
-        // value found(located in minValue)
-
-        array[minCount] = array[seek];
-        array[seek] = minValue;
-
-        //displayArray(array,SIZE);
-    }
+        //swap
+        array[min_index] = array[start];
+        array[start] = min_value;
 }
